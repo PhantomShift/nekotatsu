2d642cf2a4f07cff9a9539c99f47cc3a09ddab4f
PhantomShift <83253309+PhantomShift@users.noreply.github.com>
2024-02-13 18:27:09 -0500

    Install protoc in build

--- build.rs
+++ build.rs
@@ -1,9 +1,6 @@
 extern crate prost_build;
-extern crate protobuf_src;
 
 fn main() {
-    std::env::set_var("PROTOC", protobuf_src::protoc());
-
     if let Err(e) = prost_build::compile_protos(&["src/neko.proto"],&["src/"]) {
         println!("cargo:warning=Error occured when compiling neko.proto; did you generate it with proto_gen?");
         println!("cargo:warning={}", e.to_string());
