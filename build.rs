fd5d64f893a165c366f701c07172df7492e6c718
PhantomShift <83253309+PhantomShift@users.noreply.github.com>
2024-02-13 18:07:25 -0500

    Add `protobuf_src` for `protoc` dependency

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 extern crate prost_build;
-
+extern crate protobuf_src;
 
 fn main() {
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+
     if let Err(e) = prost_build::compile_protos(&["src/neko.proto"],&["src/"]) {
         println!("cargo:warning=Error occured when compiling neko.proto; did you generate it with proto_gen?");
         println!("cargo:warning={}", e.to_string());
