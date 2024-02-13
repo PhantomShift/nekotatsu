cdc4dcc0523b1cf190b686462e51ec722f185c86
PhantomShift <83253309+PhantomShift@users.noreply.github.com>
2024-02-13 18:56:07 -0500

    Remove protoc environment variable

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 extern crate prost_build;
 
 fn main() {
-    std::env::set_var("PROTOC", "/usr/bin/protoc");
-
     if let Err(e) = prost_build::compile_protos(&["src/neko.proto"],&["src/"]) {
         println!("cargo:warning=Error occured when compiling neko.proto; did you generate it with proto_gen?");
         println!("cargo:warning={}", e.to_string());
