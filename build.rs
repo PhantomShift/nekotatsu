1ef0f1febf98c757ec9546aa4dc0323274adcb87
PhantomShift <83253309+PhantomShift@users.noreply.github.com>
2024-02-14 15:37:47 -0500

    Remove prost-build step

--- build.rs
+++
@@ -1,8 +0,0 @@
-extern crate prost_build;
-
-fn main() {
-    if let Err(e) = prost_build::compile_protos(&["src/neko.proto"],&["src/"]) {
-        println!("cargo:warning=Error occured when compiling neko.proto; did you generate it with proto_gen?");
-        println!("cargo:warning={}", e.to_string());
-    }
-}
